b43ea15ce394f6eb38e8ddc26f39c51b139d6d2c
anhtuan <anhtuan@mediatech.vn>
2022-06-22 16:49:03 +0700

    add gtag

---
+++ src/components/GoogleAnalytics.svelte
@@ -0,0 +1,12 @@
+
+<script>
+    import { writable } from 'svelte/store';
+    const { page } = stores();
+    // $: {
+    //   if (typeof gtag !== "undefined"){
+    //     gtag("config", "UA-SOMEANALYTICSID-1", {
+    //       page_path: $page.path
+    //     });
+    //   }
+    // }
+  </script>
